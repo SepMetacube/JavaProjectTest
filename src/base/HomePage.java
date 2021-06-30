829ba552c1be22318c10bf339cc94df9b0f72c9b
Sepmetacube <hemant.gyanvihar@gmail.com>
2021-06-30 16:09:04 +0530

    Second Commit

--- src/base/HomePage.java
+++ src/base/HomePage.java
@@ -1,5 +1,8 @@
 package base;
 
 public class HomePage {
-
+ public static void main(String[] args) {
+	System.out.println("This is homepage class");
+	System.out.println("This is a second line for home page.");
+}
 }
--- src/base/LoginPage.java
+++ src/base/LoginPage.java
@@ -3,5 +3,6 @@ package base;
 public class LoginPage {
 public static void main(String[] args) {
 	System.out.println("This is login page class.");
+	System.out.println("This is a second line");
 }
 }

2c14f461df3f8042806af2209f74a6e87753bb73
Sepmetacube <hemant.gyanvihar@gmail.com>
2021-06-30 16:15:43 +0530

    Third commit

--- src/base/LoginPage.java
+++ src/base/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
 public static void main(String[] args) {
 	System.out.println("This is login page class.");
 	System.out.println("This is a second line");
+	System.out.println("This is a third line.");
 }
 }
